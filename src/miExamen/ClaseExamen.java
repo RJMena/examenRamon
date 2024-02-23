453278fda3c07fe9d5edb56baa73a293f481cbff
RJMena <LMF37464@educastur.es>
2024-02-23 19:17:40 +0100

    Añadidos comentarios de documentación en claseExamen.

--- src/miExamen/ClaseExamen.java
+++ src/miExamen/ClaseExamen.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * <h1>Entornos de desarrollo UT3</h1>
+ * <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo</p>
+ * @link https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU 
+* 
  */
 package miExamen;
 
 /**
  *
- * @author alu20v
+ * @author Ramon Menasalvas Rodriguez
+ * @version 1.0.0
+ * @since 23/02/2024
  */
 public class ClaseExamen {
-    
+     public static void main(String[] args) {
+         System.out.println("Estoy haciendo el examen de la UT3");
+     }
+     
 }
